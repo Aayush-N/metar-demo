9dfcf89ce76f3daca59a14c88c2c0e1941d63add
Aayush-N <nandkeolyar.aayush@gmail.com>
2022-03-19 21:18:35 +0530

    Add static files, make debug true in env file

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env.bool("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = ['172.105.63.176']
+ALLOWED_HOSTS = ['172.105.63.176', 'localhost']
 
 
 # Application definition
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
